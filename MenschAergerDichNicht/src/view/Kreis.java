fb575551afbafc4c08562f87e61a6beb8c853c14
jasse <jasse@jasse>
2015-05-04 12:33:11 +0200

    Kreis noch ohne Stern fuer die Schutzfelder

--- MenschAergerDichNicht/src/view/Kreis.java
+++ MenschAergerDichNicht/src/view/Kreis.java
@@ -3,7 +3,6 @@ package view;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
 
 import javax.swing.JPanel;
 
@@ -18,16 +17,9 @@ class Kreis extends JPanel {
 	  }
 	  public void paintComponent(Graphics g) {
 		  Graphics2D g2d = (Graphics2D) g;
-		  g2d.setBackground(Color.BLACK);
-		  //g2d.setColor(color);
-		  g2d.drawOval(0, 0, this.getWidth(), this.getHeight());
+		  g2d.setColor(Color.BLACK);
+		  g2d.drawOval(0, 0, this.getHeight(), this.getHeight());
 		  g2d.setColor(color);
-		  g2d.fillOval(0, 0, this.getWidth(), this.getHeight());
-		  int mx = 50;
-		  int my = 50;
-		  int r = 2000;
-		  Ellipse2D.Double kreis = new Ellipse2D.Double(mx-r, my-r, 2*r, 2*r);
-		  
-		  g2d.draw(kreis);
+		  g2d.fillOval(0, 0, this.getHeight(), this.getHeight());
 	    } 
 }
